068913c6ad8fe689d79d9e478fad4d21c4fae91c
MartinPoupa <58166333+MartinPoupa@users.noreply.github.com>
2020-02-25 11:55:53 +0100

    CCV1.1.1

--- CCV11.X/nbproject/Makefile-default.mk
+++ CCV11.X/nbproject/Makefile-default.mk
@@ -57,17 +57,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=/Users/martinpoupa/Documents/GitHub/PIC33/main.X/source_code/src/main.c
+SOURCEFILES_QUOTED_IF_SPACED=source_code/src/main.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/_ext/234506171/main.o
-POSSIBLE_DEPFILES=${OBJECTDIR}/_ext/234506171/main.o.d
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/source_code/src/main.o
+POSSIBLE_DEPFILES=${OBJECTDIR}/source_code/src/main.o.d
 
 # Object Files
-OBJECTFILES=${OBJECTDIR}/_ext/234506171/main.o
+OBJECTFILES=${OBJECTDIR}/source_code/src/main.o
 
 # Source Files
-SOURCEFILES=/Users/martinpoupa/Documents/GitHub/PIC33/main.X/source_code/src/main.c
+SOURCEFILES=source_code/src/main.c
 
 
 
@@ -95,20 +95,20 @@ MP_LINKER_FILE_OPTION=,--script=p33EV32GM002.gld
 # ------------------------------------------------------------------------------------
 # Rules for buildStep: compile
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
-${OBJECTDIR}/_ext/234506171/main.o: /Users/martinpoupa/Documents/GitHub/PIC33/main.X/source_code/src/main.c  nbproject/Makefile-${CND_CONF}.mk
-	@${MKDIR} "${OBJECTDIR}/_ext/234506171" 
-	@${RM} ${OBJECTDIR}/_ext/234506171/main.o.d 
-	@${RM} ${OBJECTDIR}/_ext/234506171/main.o 
-	${MP_CC} $(MP_EXTRA_CC_PRE)  /Users/martinpoupa/Documents/GitHub/PIC33/main.X/source_code/src/main.c  -o ${OBJECTDIR}/_ext/234506171/main.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/_ext/234506171/main.o.d"      -g -D__DEBUG -D__MPLAB_DEBUGGER_SIMULATOR=1  -mno-eds-warn  -omf=elf -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp=${DFP_DIR}/xc16
-	@${FIXDEPS} "${OBJECTDIR}/_ext/234506171/main.o.d" $(SILENT)  -rsi ${MP_CC_DIR}../ 
+${OBJECTDIR}/source_code/src/main.o: source_code/src/main.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}/source_code/src" 
+	@${RM} ${OBJECTDIR}/source_code/src/main.o.d 
+	@${RM} ${OBJECTDIR}/source_code/src/main.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  source_code/src/main.c  -o ${OBJECTDIR}/source_code/src/main.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/source_code/src/main.o.d"      -g -D__DEBUG -D__MPLAB_DEBUGGER_SIMULATOR=1  -mno-eds-warn  -omf=elf -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp=${DFP_DIR}/xc16
+	@${FIXDEPS} "${OBJECTDIR}/source_code/src/main.o.d" $(SILENT)  -rsi ${MP_CC_DIR}../ 
 	
 else
-${OBJECTDIR}/_ext/234506171/main.o: /Users/martinpoupa/Documents/GitHub/PIC33/main.X/source_code/src/main.c  nbproject/Makefile-${CND_CONF}.mk
-	@${MKDIR} "${OBJECTDIR}/_ext/234506171" 
-	@${RM} ${OBJECTDIR}/_ext/234506171/main.o.d 
-	@${RM} ${OBJECTDIR}/_ext/234506171/main.o 
-	${MP_CC} $(MP_EXTRA_CC_PRE)  /Users/martinpoupa/Documents/GitHub/PIC33/main.X/source_code/src/main.c  -o ${OBJECTDIR}/_ext/234506171/main.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/_ext/234506171/main.o.d"      -mno-eds-warn  -g -omf=elf -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp=${DFP_DIR}/xc16
-	@${FIXDEPS} "${OBJECTDIR}/_ext/234506171/main.o.d" $(SILENT)  -rsi ${MP_CC_DIR}../ 
+${OBJECTDIR}/source_code/src/main.o: source_code/src/main.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}/source_code/src" 
+	@${RM} ${OBJECTDIR}/source_code/src/main.o.d 
+	@${RM} ${OBJECTDIR}/source_code/src/main.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  source_code/src/main.c  -o ${OBJECTDIR}/source_code/src/main.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/source_code/src/main.o.d"      -mno-eds-warn  -g -omf=elf -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp=${DFP_DIR}/xc16
+	@${FIXDEPS} "${OBJECTDIR}/source_code/src/main.o.d" $(SILENT)  -rsi ${MP_CC_DIR}../ 
 	
 endif
 
