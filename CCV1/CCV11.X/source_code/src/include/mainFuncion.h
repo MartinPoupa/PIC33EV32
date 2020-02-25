aa288114b244853fe62c17a6675430bab376383d
MartinPoupa <58166333+MartinPoupa@users.noreply.github.com>
2020-02-25 12:22:58 +0100

    CCV1.1.1
    file reorganization

---
+++ CCV1/CCV11.X/source_code/src/include/mainFuncion.h
@@ -0,0 +1,15 @@
+#define  OSCILACION_FREQUENCY 7.37
+#define  REPETION_NOP 7.37
+/*
+void delay(unsigned int milis){
+    for (unsigned int i = 0; i < milis; i++) {
+        for (unsigned int j = 0; j < 3685; j++) {
+            asm(" nop ");
+        }
+    }
+}
+*/
+int pokus( int milis){
+return milis;
+
+}
---
+++ pattern/main.X/source_code/src/main.c
@@ -0,0 +1,41 @@
+/*
+* File:   main.c
+* Author: Martin Poupa
+*
+* Created on 2. prosince 2018, 16:43
+*/
+
+
+
+#include "p33EV32GM002.h"
+
+
+
+
+
+
+#pragma config FNOSC = FRC     //  pracujeme s FRC oscilatorem 7.37MHz
+#pragma config OSCIOFNC = ON   //OSC2 is general purpose digital I/O pin
+#pragma config IOL1WAY = ON    //Allow Only One reconfiguration pro PPS
+#pragma config FWDTEN = OFF	   // WDT and SWDTEN Disabled    Watchdog vypnut
+
+
+
+
+void  main() {
+
+    asm(" nop " );
+    asm(" nop " );
+    asm(" nop " );
+    asm(" nop " );
+    asm(" nop " );
+
+    while(1) {
+
+        asm(" nop " );
+        asm(" nop " );
+        asm(" nop " );
+        asm(" nop " );
+
+    }
+}
