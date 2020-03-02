820510c7247d22fbe0f4fe610e9a8fc879bd061a
MartinPoupa <58166333+MartinPoupa@users.noreply.github.com>
2020-03-02 11:00:18 +0100

    CVV1.2.1

---
+++ pattern/PWM.X/src/asm/mainFuncion.s
@@ -0,0 +1,30 @@
+
+            .include "p33EV32GM002.inc"
+
+
+		    ;.bss
+		    ;.text
+
+		    .global _delay
+_delay:		    do W0, stop
+			     repeat #7368
+			              nop
+stop:		     nop
+		         return
+
+
+
+
+
+
+;            .global _pinMode
+;_pinMode:     do W0, stop
+;                repeat #7365
+;                    nop
+;stop:           nop
+;            return
+
+
+
+
+            .end
