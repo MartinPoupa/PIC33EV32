b8054dd2172d1928b0ca9d44f03ad4f2708cf30a
MartinPoupa <58166333+MartinPoupa@users.noreply.github.com>
2020-03-06 12:01:37 +0100

    ADConvertor

---
+++ CCV2/CCV22.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=CCV22.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/CCV22.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=ccv22.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/ccv22.x.tar
