9a852b173fff82ae78084f6a1a5a5d892deab050
MartinPoupa <58166333+MartinPoupa@users.noreply.github.com>
2020-03-23 15:57:12 +0100

    spi

--- pattern/SPI2.X/src/main.c
+++ pattern/SPI2.X/src/main.c
@@ -28,7 +28,7 @@ int main() {
       pinAD(0, DIGITAL);
       //FrequencyT2(35000);
       T2CON = 0x8000;
-      PR2 = 70;
+      PR2 = 90;
       IFS0bits.T2IF = 0;
       IEC0bits.T2IE = 1;
       setDA();
