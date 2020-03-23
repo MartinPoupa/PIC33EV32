aa731a64be6226bbc7393bd94aaf2b206ff6ecf3
MartinPoupa <58166333+MartinPoupa@users.noreply.github.com>
2020-03-23 17:59:31 +0100

    CCV32

---
+++ CCV3/CCV32.X/src/main.c
@@ -0,0 +1,40 @@
+/*
+* File:   main.c
+* Author: Matej Kraus and Martin Poupa
+*
+*/
+
+#include "p33EV32GM002.h"
+#include "include/mainFuncion.h"
+
+#pragma config FNOSC = FRC     //  pracujeme s FRC oscilatorem 7.37MHz
+#pragma config OSCIOFNC = ON   //OSC2 is general purpose digital I/O pin
+#pragma config IOL1WAY = ON    //Allow Only One reconfiguration pro PPS
+#pragma config FWDTEN = OFF	   // WDT and SWDTEN Disabled    Watchdog vypnut
+
+
+int voltageDA = 0;
+
+void __attribute__((interrupt, auto_psv)) _T2Interrupt(void) {
+    IFS0bits.T2IF = 0;
+    if (voltageDA >= 0x0fff) {
+        voltageDA = 0;
+    }
+    voltageDA++;
+    DA(A, voltageDA);
+
+}
+
+
+int main() {
+      FrequencyT2(10000);
+      setDA();
+      startInterrupts();
+      DA(A, 0);
+
+
+    while (1) {
+        delay(1);
+    }
+      return 0;
+}
