1e4391a6d4359e39c0c7dcf5e0aaaacd646d4006
MartinPoupa <58166333+MartinPoupa@users.noreply.github.com>
2020-03-23 17:29:16 +0100

    CCV31

---
+++ CCV3/CCV31.X/src/main.c
@@ -0,0 +1,27 @@
+/*
+* File:   main.c
+* Author: Matej Kraus and Martin Poupa
+*
+*/
+
+#include "p33EV32GM002.h"
+#include "include/mainFuncion.h"
+
+#pragma config FNOSC = FRC     //  pracujeme s FRC oscilatorem 7.37MHz
+#pragma config OSCIOFNC = ON   //OSC2 is general purpose digital I/O pin
+#pragma config IOL1WAY = ON    //Allow Only One reconfiguration pro PPS
+#pragma config FWDTEN = OFF	   // WDT and SWDTEN Disabled    Watchdog vypnut
+
+
+
+int main() {
+      setDA();                    //Nastaveni DA prevodniku
+
+    while (1) {
+        delay(2000);
+        DA(A, 910);
+        delay(2000);
+        DA(A, 2730);
+    }
+      return 0;
+}
--- pattern/citac.X/src/include/mainFuncion.h
+++ pattern/citac.X/src/include/mainFuncion.h
@@ -1,4 +1,4 @@
-#define  OSCILACION_FREQUENCY 7.37
+#define  OSCILACION_FREQUENCY 7370000
 #define  CYCLE_FREQUENCY (OSCILACION_FREQUENCY / 2)
 
 #define OUTPUT 0
@@ -7,7 +7,7 @@
 #define DIGITAL 0
 #define ANALOG 1
 
-#define A 1
+#define A 0x8000
 #define B 0
 
 #define LOW 0
@@ -17,31 +17,31 @@
 
 void delay(int t);                              // it waits t seconds
 
-void pinMode(int pinNumber, int set);           // set OUTPUT/INPUT
+void pinMode(int pinNumber, int set);            // set OUTPUT/INPUT
 void pinAD(int pinNumber, int set);             // set DIGITAL/ANALOG
 
 void digitalWrite(int pinNumber, int set);      // set LOW/HIGH
 
 void setDA();                                   // sets the DA converter
-void DA (int chanel, int value );               // sends value to DA converter
+void DA (int chanel, int value);               // sends value to DA converter
 
 void startInterrupts();                         // enable interrupt
-void FrequencyT2(int frequency) {
+void FrequencyT2(unsigned int frequency) {
     if(frequency > 64){
-        T2CON = 0xA000; // 1
-        PR2 = (CYCLE_FREQUENCY * 1000000) / frequency ;
+        T2CON = 0x8000; // 1
+        PR2 = CYCLE_FREQUENCY / frequency ;
         IFS0bits.T2IF = 0;
         IEC0bits.T2IE = 1;
     }
     else if(frequency <= 64 && frequency > 8){
-        T2CON = 0xA010; // 8
-        PR2 = ((CYCLE_FREQUENCY * 1000000) / 8) / frequency ;
+        T2CON = 0x8010; // 8
+        PR2 = (CYCLE_FREQUENCY / 8) / frequency ;
         IFS0bits.T2IF = 0;
         IEC0bits.T2IE = 1;
     }
     else if(frequency <= 8){
-        T2CON = 0xA020; // 64
-        PR2 = ((CYCLE_FREQUENCY * 1000000) / 64) / frequency ;
+        T2CON = 0x8020; // 64
+        PR2 = (CYCLE_FREQUENCY / 64) / frequency ;
         IFS0bits.T2IF = 0;
         IEC0bits.T2IE = 1;
     }
