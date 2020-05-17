294434ba994ed7d84741a143697f7c5ac57256ef
MartinPoupa <58166333+MartinPoupa@users.noreply.github.com>
2020-05-17 22:55:22 +0200

    CCV8

--- CCV8/CCV81.X/src/main.c
+++ CCV8/CCV81.X/src/main.c
@@ -15,40 +15,36 @@
 #pragma config IOL1WAY = ON    //Allow Only One reconfiguration pro PPS
 #pragma config FWDTEN = OFF	   // WDT and SWDTEN Disabled    Watchdog vypnut
 
-tik = 0;
-
-void __attribute__((interrupt, auto_psv)) _T2Interrupt(void) {
-    IFS0bits.T2IF = 0;
-    PDC1 = tik;
-    if(tik <= 3684){
-        tik++;
-    }
-    else{
-        tik = 0;
-    }
-
-}
-
-
-
-
 int main() {
-
+    //pwm
     pinMode(B, 15, OUTPUT);
     pinMode(B, 14, OUTPUT);
-    pinMode(B, 13, OUTPUT);
-    pinMode(B, 12, OUTPUT);
 
+    //control pins
+    pinMode(B, 0, INPUT);
+    pinMode(B, 1, INPUT);
+
+    //pwm
     pinAD(B, 15, DIGITAL);
     pinAD(B, 14, DIGITAL);
-    pinAD(B, 13, DIGITAL);
-    pinAD(B, 12, DIGITAL);
+
+    //control pins
+    pinAD(B, 0, DIGITAL);
+    pinAD(B, 1, DIGITAL);
+
+    //control pins
+    pinPull(B, 0, UP);
+    pinPull(B, 1, UP);
+
+
+
+    int pwmF = 3684;
 
     PTCON2 = 0;
-    PTPER = 3684;
+    PTPER = pwmF;
 
     PHASE1 = 0;
-    PDC1 = 0;
+    PDC1 = pwmF / 5;
     DTR1 = 0;
     ALTDTR1 = 0;
 
@@ -58,11 +54,24 @@ int main() {
 
     PTCON = 0x8000;
 
-    FrequencyT2(100);
-    startInterrupts();
-
     while (1) {
         delay(1);
+        if(digitalRead(B, 0)){
+            if(digitalRead(B, 1)){
+                PDC1 = (pwmF / 5) * 4;
+            }
+            else{
+                PDC1 = (pwmF / 5) * 3;
+            }
+        }
+        else{
+            if(digitalRead(B, 1)){
+                PDC1 = (pwmF / 5) * 2;
+            }
+            else{
+                PDC1 = (pwmF / 5) * 1;
+            }
+        }
     }
     return 0;
 }
