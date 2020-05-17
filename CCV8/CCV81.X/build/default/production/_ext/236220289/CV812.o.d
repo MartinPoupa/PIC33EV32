88c0eb336b12c08302a8f6efe43d8fd9718b9aa7
MartinPoupa <58166333+MartinPoupa@users.noreply.github.com>
2020-05-17 21:48:01 +0200

    CCV8

---
+++ CCV8/CCV81.X/build/default/production/_ext/236220289/CV812.o.d
@@ -0,0 +1,3 @@
+build/default/production/_ext/236220289/CV812.o: \
+ /Applications/microchip/xc16/v1.50/bin/bin/../../support/dsPIC33E/inc/p33EV32GM002.inc \
+ /Users/martinpoupa/programi/H8/CV812.s
