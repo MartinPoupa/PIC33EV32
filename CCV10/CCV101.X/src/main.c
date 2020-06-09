90eb716e732a6c9f8a41b3b370d0fb3f6845d360
MartinPoupa <58166333+MartinPoupa@users.noreply.github.com>
2020-06-09 13:45:51 +0200

    CCV101

--- CCV10/CCV101.X/src/main.c
+++ CCV10/CCV101.X/src/main.c
@@ -7,8 +7,16 @@
 #include "p33EV32GM002.h"
 #include "include/mainFuncion.h"
 
+//#pragma config FNOSC = FRC     //  pracujeme s FRC oscilatorem 7.37MHz
+//#pragma config OSCIOFNC = ON   //OSC2 is general purpose digital I/O pin
+//#pragma config IOL1WAY = ON    //Allow Only One reconfiguration pro PPS
+//#pragma config FWDTEN = OFF	   // WDT and SWDTEN Disabled    Watchdog vypnut
+
 #pragma config FNOSC = FRC     //  pracujeme s FRC oscilatorem 7.37MHz
-#pragma config OSCIOFNC = ON   //OSC2 is general purpose digital I/O pin
+#pragma config IESO  = OFF     //  Start up device with user selected oscillator source , u nas FRC
+#pragma config  FCKSM  =   CSECMD  //  Clock Switching is enabled,Fail-safe Clock Monitor is disabled
+#pragma config POSCMD = NONE	//Primary Oscillator disabled
+#pragma config OSCIOFNC = OFF   //OSC2 OSC2 is clock output   RA3   pin 10
 #pragma config IOL1WAY = ON    //Allow Only One reconfiguration pro PPS
 #pragma config FWDTEN = OFF	   // WDT and SWDTEN Disabled    Watchdog vypnut
 
@@ -47,9 +55,38 @@ int main() {
     pinAD(B, 0, ANALOG);
     pinPull(B, 0, DOWN);
 
+    pinMode(B, 4, OUTPUT);
+    pinAD(B, 4, DIGITAL);
+
     setDA();
     FrequencyT2(20000);
-    startInterrupts();
+
+    __builtin_write_OSCCONL(OSCCON & 0xBF) ;
+    RPOR1 = 0x0031;
+    __builtin_write_OSCCONL(OSCCON |  0x40) ;
+
+
+
+    REFOCON = 0x8000;
+
+    CLKDIV = 0;
+    //CLKDIVbits.FRCDIV =  0;
+    PLLFBD=31;
+    CLKDIVbits.PLLPOST=1;
+    CLKDIVbits.PLLPRE=0;
+
+    INTCON2bits.GIE = 0;
+    __builtin_write_OSCCONH(0x01);
+    __builtin_write_OSCCONL(OSCCON | 0x01);
+
+    INTCON2bits.GIE = 1;
+    //startInterrupts();
+
+
+    while (OSCCONbits.LOCK != 1){
+        delay(1);
+    }
+
     while (1) {
         delay(1);
     }
